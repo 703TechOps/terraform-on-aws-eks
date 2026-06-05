4e6c1c03fe73bed9c1660e5377d71fe7438cfba8
703TechOps <141341987+703TechOps@users.noreply.github.com>
2026-06-05 17:20:09 -0400

    Update ec2-instance.tf

--- 02-Terraform-Basics/02-02-Terraform-Command-Basics/terraform-manifests/ec2-instance.tf
+++ 02-Terraform-Basics/02-02-Terraform-Command-Basics/terraform-manifests/ec2-instance.tf
@@ -16,6 +16,9 @@ provider "aws" {
 
 # Resource Block
 resource "aws_instance" "ec2demo" {
-  ami           = "ami-0533f2ba8a1995cf9" # Amazon Linux in us-east-1, update as per your region
-  instance_type = "t2.micro"
+  ami           = "ami-00e801948462f718a" # Amazon Linux in us-east-1, update as per your region
+  instance_type = "t3.micro"
+
+  # Link your existing AWS Key Pair here
+  key_name      = "703demo-keypair.pem"
 }
