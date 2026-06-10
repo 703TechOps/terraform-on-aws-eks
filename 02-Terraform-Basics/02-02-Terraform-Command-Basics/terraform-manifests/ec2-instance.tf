a28edcc91b0c83f80e532588f416f4187b4a1085
703TechOps <141341987+703TechOps@users.noreply.github.com>
2026-06-10 18:13:22 -0400

    adding httpd and hostname

--- 02-Terraform-Basics/02-02-Terraform-Command-Basics/terraform-manifests/ec2-instance.tf
+++ 02-Terraform-Basics/02-02-Terraform-Command-Basics/terraform-manifests/ec2-instance.tf
@@ -18,7 +18,29 @@ provider "aws" {
 resource "aws_instance" "ec2demo" {
   ami           = "ami-00e801948462f718a" # Amazon Linux in us-east-1, update as per your region
   instance_type = "t3.micro"
-  
+
   # Link your existing AWS Key Pair here
   key_name      = "703demo-keypair"
+
+  # Sets the hostname, installs Apache, and writes the custom index.html file
+  user_data = <<-EOF
+              #!/bin/bash
+              hostnamectl set-hostname c2-demo-terraform"
+              
+              # Install Apache Web Server
+              dnf update -y
+              dnf install -y httpd
+              
+              # Start Apache and enable it to start on system boot
+              systemctl start httpd
+              systemctl enable httpd
+              
+              # Write the custom HTML content
+              echo "welcome to AK world" > /var/www/html/index.html
+              EOF
+
+  # Sets the display name in the AWS Console
+  tags = {
+    Name = "ec2-demo-terraform"
+  }
 }
